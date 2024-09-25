3fb83d18d47fce71e2073010845ea230d5c82b5d
Shymastic <shymastic@gmail.com>
2024-09-25 14:27:50 +0700

    add swagger to azure

--- ClassBookingRoom_API/Program.cs
+++ ClassBookingRoom_API/Program.cs
@@ -70,7 +70,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
