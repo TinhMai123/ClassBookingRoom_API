70d5c81abb4dfedf075341e1ba10455624956868
huyngse <ngogiahuy775309@gmail.com>
2024-09-27 18:41:12 +0700

    Add Auth Controller

--- ClassBookingRoom_API/Controllers/UserController.cs
+++ ClassBookingRoom_API/Controllers/UserController.cs
@@ -1,13 +1,6 @@
 ﻿using ClassBookingRoom_BusinessObject.DTO.User;
-using ClassBookingRoom_BusinessObject.Mappers;
-using ClassBookingRoom_BusinessObject.Models;
 using ClassBookingRoom_Service.IServices;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 
 namespace ClassBookingRoom_API.Controllers
 {
@@ -80,73 +73,6 @@ namespace ClassBookingRoom_API.Controllers
                 return BadRequest(ex.Message);
             }
         }
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginDTO login)
-        {
-            try
-            {
-                var token = await _userService.Login(login);
-                if (token != null) return Ok(token);
-                else return NotFound("User Not Found");
-            }
-            catch (Exception ex) { return BadRequest(ex.Message); }
-        }
-        [HttpGet("get-user-info")]
-        public IActionResult GetUserInfo()
-        {
-            var identity = HttpContext.User.Identity as ClaimsIdentity;
-
-            if (identity != null)
-            {
-                var claims = identity.Claims;
-                var email = claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
-                var role = claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
-                if (email != null && role != null)
-                {
-                    var user = _userService.GetUserByEmailAsync(email);
-                    return Ok(user);
-                }
-                else
-                {
-                    return Unauthorized();
-                }
-            }
-
-            return Unauthorized();
-        }
-        [HttpGet("token")]
-        public async Task<ActionResult<UserDTO>> CheckToken()
-        {
-            Request.Headers.TryGetValue("Authorization", out var token);
-            token = token.ToString().Split()[1];
-            // Here goes your token validation logic
-            if (string.IsNullOrWhiteSpace(token))
-            {
-                return BadRequest("Authorization header is missing or invalid.");
-            }
-            // Decode the JWT token
-            var handler = new JwtSecurityTokenHandler();
-            var jwtToken = handler.ReadJwtToken(token);
-
-            // Check if the token is expired
-            if (jwtToken.ValidTo < DateTime.UtcNow)
-            {
-                return BadRequest("Token has expired.");
-            }
-
-            string? email = jwtToken.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
-            if (email is null)
-            {
-                return StatusCode(500);
-            }
-            var user = await _userService.GetUserByEmailAsync(email);
-            if (user == null)
-            {
-                return BadRequest("email is in valid");
-            }
-
-            // If token is valid, return success response
-            return Ok(user);
-        }
+       
     }
 }
--- ClassBookingRoom_API/Program.cs
+++ ClassBookingRoom_API/Program.cs
@@ -76,7 +76,7 @@ builder
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
             )
         };
     });
