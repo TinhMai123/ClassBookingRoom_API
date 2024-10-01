00c6e2c2d328ae73facb40ac71866cc1b7ac8790
huyngse <ngogiahuy775309@gmail.com>
2024-10-01 13:49:36 +0700

    Remove Business Object layer & appsettings

---
+++ ClassBookingRoom_Repository/DTO/Cohort/CohortDTO.cs
@@ -0,0 +1,18 @@
+﻿using ClassBookingRoom_BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBookingRoom_BusinessObject.DTO.Cohort
+{
+    public class CohortDTO
+    {
+        public int Id { get; set; }
+        public string? CohortCode { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public DateTime? DeleteAt { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ ClassBookingRoom_Repository/DTO/User/GetUserTypeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBookingRoom_BusinessObject.DTO.User
+{
+    public class GetUserTypeDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string Role { get; set; } = string.Empty;
+    }
+}
