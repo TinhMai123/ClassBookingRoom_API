e37f4a5f66298db6b188d070798f146660613585
huyngse <ngogiahuy775309@gmail.com>
2024-10-08 15:16:44 +0700

    Login Google Controller

---
+++ ClassBookingRoom_Repository/RequestModels/Auth/LoginGoogleRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBookingRoom_Repository.RequestModels.Auth
+{
+    public class LoginGoogleRequest
+    {
+        public required string AccessToken { get; set; }
+        public required string Role { get; set; }
+    }
+}
--- ClassBookingRoom_Service/IServices/IUserService.cs
+++ ClassBookingRoom_Service/IServices/IUserService.cs
@@ -1,6 +1,7 @@
 ﻿using ClassBookingRoom_Repository.RequestModels.Auth;
 using ClassBookingRoom_Repository.RequestModels.User;
 using ClassBookingRoom_Repository.ResponseModels.User;
+using FirebaseAdmin.Auth;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,7 @@ namespace ClassBookingRoom_Service.IServices
         Task<bool> DeleteUserAsync(int id);
         Task<GetUserTypeResponseModel?> GetUserTypeByEmailAsync(string email);
         Task<UserDetailResponseModel?> GetUserByEmailAsync(string email);
+        Task<string?> LoginGoogle(FirebaseToken token, string role);
         Task<string?> Login(LoginRequestModel request);
         Task<List<UserResponseModel>> GetAllUser();
         Task<(List<UserResponseModel>, int)> SearchUser(SearchUserQuery query);
