4b0cce2008286d81b4b030f01ecd926300f674fb
Shymastic <shymastic@gmail.com>
2024-10-09 12:55:10 +0700

    fixing database

--- ClassBookingRoom_Repository/Models/User.cs
+++ ClassBookingRoom_Repository/Models/User.cs
@@ -11,8 +11,7 @@ namespace ClassBookingRoom_Repository.Models
     public class User : BaseModel
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
