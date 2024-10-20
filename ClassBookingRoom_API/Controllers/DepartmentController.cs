02eb5a15f582e99b2aa9c684a34fc8b618ccb525
huyngse <ngogiahuy775309@gmail.com>
2024-10-20 15:33:40 +0700

    Merge Activity controller to Department controller

--- ClassBookingRoom_API/Controllers/DepartmentController.cs
+++ ClassBookingRoom_API/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using ClassBookingRoom_Repository.RequestModels.Department;
+﻿using ClassBookingRoom_Repository.RequestModels.Activity;
+using ClassBookingRoom_Repository.RequestModels.Department;
+using ClassBookingRoom_Repository.ResponseModels.Activity;
 using ClassBookingRoom_Repository.ResponseModels.Department;
 using ClassBookingRoom_Service.IServices;
 using Microsoft.AspNetCore.Mvc;
@@ -10,11 +12,13 @@ namespace ClassBookingRoom_API.Controllers
     [ApiController]
     public class DepartmentController : Controller
     {
+        private readonly IActivityService _activityService;
         private readonly IDepartmentService _departmentService;
 
-        public DepartmentController(IDepartmentService departmentService)
+        public DepartmentController(IDepartmentService departmentService, IActivityService activityService)
         {
             _departmentService = departmentService;
+            _activityService = activityService;
         }
         [HttpGet]
         public async Task<ActionResult<List<DepartmentResponseModel>>> GetALl()
@@ -23,7 +27,7 @@ namespace ClassBookingRoom_API.Controllers
             return Ok(departments);
         }
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<DepartmentResponseModel>> GetById([FromRoute]int id)
+        public async Task<ActionResult<DepartmentResponseModel>> GetById([FromRoute] int id)
         {
             var department = await _departmentService.GetById(id);
             if (department == null)
@@ -33,7 +37,7 @@ namespace ClassBookingRoom_API.Controllers
             return Ok(department);
         }
         [HttpPut("{id:int}")]
-        public async Task<IActionResult> Update([FromRoute]int id, [FromBody]UpdateDepartmentRequestModel dto)
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateDepartmentRequestModel dto)
         {
             var result = await _departmentService.Update(id, dto);
             if (result)
@@ -43,7 +47,7 @@ namespace ClassBookingRoom_API.Controllers
             return BadRequest();
         }
         [HttpPost]
-        public async Task<IActionResult> Create( [FromBody] CreateDepartmentRequestModel dto)
+        public async Task<IActionResult> Create([FromBody] CreateDepartmentRequestModel dto)
         {
             var result = await _departmentService.Create(dto);
             if (result)
@@ -61,6 +65,63 @@ namespace ClassBookingRoom_API.Controllers
                 return Ok();
             }
             return NotFound();
+        }
+        // ACTIVITIES
+
+        [HttpGet("activites/{id:int}")]
+        public async Task<ActionResult<ActivityResponseModel>> GetActivityById([FromRoute] int id)
+        {
+            var activity = await _activityService.GetById(id);
+            if (activity == null) { return NotFound(); }
+            return Ok(activity);
+        }
+
+        [HttpGet("activities")]
+        public async Task<ActionResult<List<ActivityResponseModel>>> GetAllActivities()
+        {
+            var list = await _activityService.GetAll();
+            return Ok(list);
+        }
+
+        [HttpPost("activities")]
+        public async Task<ActionResult> CreateActivity(CreateActivityRequestModel add)
+        {
+            var result = await _activityService.CreateAsync(add);
+            if (result)
+            {
+                return Ok();
+            } else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPut("activities/{id:int}")]
+        public async Task<ActionResult> UpdateActivity([FromRoute] int id, [FromBody] UpdateActivityRequestModel update)
+        {
+            var result = await _activityService.UpdateAsync(id, update);
+
+            if (result)
+            {
+                return Ok();
+            } else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("activities/{id:int}")]
+        public async Task<ActionResult> DeleteActivity([FromRoute] int id)
+        {
+            var result = await _activityService.DeleteAsync(id);
+            if (result)
+            {
+                return Ok(result);
+            } else
+            {
+                return BadRequest();
+            }
         }
     }
 }
