a6ff650ac372b4ec6252d5779bad373442c85499
huyngse <ngogiahuy775309@gmail.com>
2024-10-30 12:51:36 +0700

    Update CORS

--- ClassBookingRoom_API/Program.cs
+++ ClassBookingRoom_API/Program.cs
@@ -111,7 +111,7 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddAuthorization();
 builder.Services.AddCors(options => options.AddDefaultPolicy(policyBuilder =>
         policyBuilder
-        .WithOrigins("http://localhost:5173", "https://fu-booking-room.vercel.app")
+        .WithOrigins("http://localhost:5173", "https://class-booking-room.vercel.app")
         .AllowAnyMethod()
         .AllowAnyHeader()
         .WithExposedHeaders("X-Total-Count", "X-Current-Page", "X-Total-Pages")
